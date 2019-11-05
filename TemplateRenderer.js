777727ece0438793f6c7f925dd6bae844cbbc26d
Jayson Faderanga <jayson.faderanga@gmail.com>
2019-11-06 11:17:43 +1300

    Create TemplateRenderer.js

---
+++ TemplateRenderer.js
@@ -0,0 +1,11 @@
+({
+	afterRender: function (component, helper) {
+        this.superAfterRender();
+        
+        window.setTimeout(
+            $A.getCallback(function () {
+                $A.get("e.force:closeQuickAction").fire();
+            }), 0
+        );
+    }
+})
