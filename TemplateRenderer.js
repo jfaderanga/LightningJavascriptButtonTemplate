e5aa6b5dfab2ef1758c364df90be984e84d6b4c1
Jayson Faderanga <jayson.faderanga@gmail.com>
2019-11-08 01:10:48 +1300

    Update TemplateRenderer.js

--- TemplateRenderer.js
+++ TemplateRenderer.js
@@ -1,11 +1,19 @@
 ({
 	afterRender: function (component, helper) {
         this.superAfterRender();
-        
-        window.setTimeout(
+
+        // we set the closeActionEvent here as it doesn't work on init
+        // then we poll till the init action is finished before we closed
+        component._interval = setInterval(
             $A.getCallback(function () {
+                
+                if (!component.get("v.doneAllAction")) return;
+
+                clearInterval(component._interval);
+
                 $A.get("e.force:closeQuickAction").fire();
-            }), 0
+
+            }), 100
         );
     }
 })
